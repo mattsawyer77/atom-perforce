bf72d3058846631d7d22946ef848ada9c80ec706
Matt Sawyer <matt.sawyer@gmail.com>
2015-02-07 17:06:49 -0800

    forgot to add new files, updated package.json

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 'use strict';
 
-var path = require('path'),
-    p4 = require('node-perforce'),
-    Q = require('q'),
-    environment = require('./lib/environment');
+var environment = require('./lib/environment'),
+    atomPerforce = require('./lib/atom-perforce');
 
 module.exports = {
     activate: function activate(/*state*/) {
@@ -28,50 +26,13 @@ module.exports = {
         })
         .catch(function(err) {
             console.error('could not load environment variables:', err);
+            // try simply making sure /usr/local/bin (the default p4 location) is in the path
+            // TODO: fallback for windows?
+            if(process.env.PATH.split(':').indexOf('/usr/local/bin') === -1) {
+                process.env.PATH = process.env.PATH + ':/usr/local/bin';
+            }
         });
 
-        atom.commands.add('atom-workspace', 'perforce:edit', function() {
-            var editor = atom.workspace.getActivePaneItem(),
-                openedBufferFilePath = path.dirname(editor.buffer.file.path),
-                openedBufferFilename = editor.buffer.file.path.replace(openedBufferFilePath + '/', ''),
-                originalCWD = process.cwd(),
-                pathChanged = false;
-
-            Q.fcall(function() {
-                if(openedBufferFilePath !== originalCWD) {
-                    // cd to the directory containing the opened file
-                    process.chdir(openedBufferFilePath);
-                    pathChanged = true;
-                }
-                else {
-                    return false;
-                }
-            })
-            // call p4 info
-            .then(Q.nfcall(p4.info))
-            .then(function(p4Info) {
-                console.log(p4Info);
-
-                // open the file for edit
-                return Q.nfcall(p4.edit, { files: [openedBufferFilename] })
-                .then(function(result) {
-                    // TODO: show something in the status bar temporarily
-                    console.log(openedBufferFilename + ' opened for edit', result);
-                })
-                .catch(function(err) {
-                    // TODO: show something in the status bar temporarily
-                    console.log('could not p4 edit file ' + openedBufferFilename, err);
-                });
-            })
-            .then(function() {
-                // reload the file, since perforce has likely chmod'd the file to be writable
-                return editor.buffer.reload();
-            })
-            .catch(function(err) {
-                console.err(err);
-            });
-        });
-
-
+        atom.commands.add('atom-workspace', 'perforce:edit', atomPerforce.edit);
     }
 };

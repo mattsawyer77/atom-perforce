9a42f20ab7d6407ad1cbeac53931e1910b5541fb
Matt Sawyer <matt.sawyer@gmail.com>
2015-02-10 14:59:57 -0800

    refactored main.js

--- main.js
+++ main.js
@@ -3,53 +3,60 @@
 var environment = require('./lib/environment'),
     atomPerforce = require('./lib/atom-perforce');
 
-module.exports = {
-    activate: function activate(/*state*/) {
-        environment.loadVarsFromEnvironment([
-            'PATH',
-            'P4CONFIG',
-            'P4DIFF',
-            'P4IGNORE',
-            'P4MERGE',
-            'P4PORT',
-            'P4USER',
-            'PAGER',
-            'PATH'
-        ])
-        .then(function() {
-            console.log('loaded environment variables');
-            atom.workspace.observeTextEditors(function(editor) {
-                // mark changes on save
-                editor.buffer.onDidSave(function(file) {
-                    atomPerforce.getChanges()
-                    .then(function(changes) {
-                        atomPerforce.showDiffMarks(file.path, changes);
-                    });
-                });
+function setupEnvironment() {
+    return environment.loadVarsFromEnvironment([
+        'PATH',
+        'P4CONFIG',
+        'P4DIFF',
+        'P4IGNORE',
+        'P4MERGE',
+        'P4PORT',
+        'P4USER',
+        'PAGER',
+        'PATH'
+    ]).catch(function(err) {
+        console.error('could not load environment variables:', err);
+        // try simply making sure /usr/local/bin (the default p4 location) is in the path
+        // TODO: fallback for windows?
+        if(process.env.PATH.split(':').indexOf('/usr/local/bin') === -1) {
+            process.env.PATH = process.env.PATH + ':/usr/local/bin';
+        }
+    });
+}
 
-                // TODO: mark changes as-you-type (and provide a config option to disable)
-
-                // mark changes on initial load
-                if(editor.getPath()) {
-                    atomPerforce.getChanges()
-                    .then(function(changes) {
-                        atomPerforce.showDiffMarks(editor.getPath(), changes);
-                    });
-                }
+function setupObservers() {
+    return atom.workspace.observeTextEditors(function(editor) {
+        // mark changes on save
+        editor.buffer.onDidSave(function(file) {
+            atomPerforce.getChanges()
+            .then(function(changes) {
+                atomPerforce.showDiffMarks(file.path, changes);
             });
-        })
-        .catch(function(err) {
-            console.error('could not load environment variables:', err);
-            // try simply making sure /usr/local/bin (the default p4 location) is in the path
-            // TODO: fallback for windows?
-            if(process.env.PATH.split(':').indexOf('/usr/local/bin') === -1) {
-                process.env.PATH = process.env.PATH + ':/usr/local/bin';
-            }
         });
 
-        atom.commands.add('atom-workspace', 'perforce:edit', atomPerforce.edit);
-        atom.commands.add('atom-workspace', 'perforce:add', atomPerforce.add);
-        atom.commands.add('atom-workspace', 'perforce:sync', atomPerforce.sync);
-        atom.commands.add('atom-workspace', 'perforce:revert', atomPerforce.revert);
+        // TODO: mark changes as-you-type (and provide a config option to disable)
+
+        // mark changes on initial load
+        if(editor.getPath()) {
+            atomPerforce.getChanges()
+            .then(function(changes) {
+                atomPerforce.showDiffMarks(editor.getPath(), changes);
+            });
+        }
+    });
+}
+
+function setupCommands() {
+    atom.commands.add('atom-workspace', 'perforce:edit', atomPerforce.edit);
+    atom.commands.add('atom-workspace', 'perforce:add', atomPerforce.add);
+    atom.commands.add('atom-workspace', 'perforce:sync', atomPerforce.sync);
+    atom.commands.add('atom-workspace', 'perforce:revert', atomPerforce.revert);
+}
+
+module.exports = {
+    activate: function activate(/*state*/) {
+        setupEnvironment()
+        .then(setupObservers)
+        .then(setupCommands);
     }
 };

256bad4e32fe28cc723957181fbfa1f67159b00e
Matt Sawyer <matt.sawyer@gmail.com>
2015-04-02 00:12:02 -0700

    implement #5 by adding a setting for the p4 command path, refactor event handling, add error when p4 command is not found

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 'use strict';
 
-var Q = require('q'),
+var path = require('path'),
     environment = require('./lib/environment'),
     atomPerforce = require('./lib/atom-perforce'),
-    defaultUnixP4Directory = '/usr/local/bin';
+    CompositeDisposable = require('atom').CompositeDisposable,
+    defaultUnixP4Directory = '/usr/local/bin',
+    defaultWindowsP4Directory = 'C:\Program Files\Perforce',
+    commandsSetup = false,
+    observers;
+
+function getDefaultP4Location() {
+    if(process.platform === 'win32') {
+        return defaultWindowsP4Directory;
+    }
+    else {
+        return defaultUnixP4Directory;
+    }
+}
 
 function setupEnvironment() {
     return environment.loadVarsFromEnvironment([
@@ -21,23 +34,24 @@ function setupEnvironment() {
         console.error('could not load environment variables:', err);
     })
     .finally(function() {
-        var pathElements;
-        // make sure /usr/local/bin (the default p4 location) is in the path
-        if(process.platform !== 'win32') {
-            pathElements = process.env.PATH.split(':');
-            if(pathElements.indexOf(defaultUnixP4Directory) === -1) {
-                pathElements.unshift(defaultUnixP4Directory);
-                process.env.PATH = pathElements.join(':');
-            }
+        var pathElements,
+            defaultPath = atom.config.get('atom-perforce.defaultP4Location') || getDefaultP4Location();
+
+        // make sure the default p4 location is in the path
+        pathElements = process.env.PATH.split(path.delimiter);
+        if(pathElements.indexOf(defaultPath) === -1) {
+            pathElements.unshift(defaultPath);
+            process.env.PATH = pathElements.join(path.delimiter);
         }
     });
 }
 
 function setupObservers() {
-    var treeObserver = new MutationObserver(function treeChanged(mutations, observer) {
+    var observers = new CompositeDisposable(),
+        treeObserver = new MutationObserver(function treeChanged(/*mutations, observer */) {
             atomPerforce.markOpenFiles();
         }),
-        options = {
+        treeObserverOptions = {
             subtree: true,
             childList: true,
             attributes: false
@@ -45,15 +59,22 @@ function setupObservers() {
 
     // monitor the tree for changes (collapsing/expanding)
     // TODO: if Atom ever publishes an event for this, use that instead
-    treeObserver.observe(document.querySelector('.tool-panel'), options);
+    treeObserver.observe(document.querySelector('.tool-panel'), treeObserverOptions);
 
-    atom.workspace.observeTextEditors(function(editor) {
+    // make this work like an Atom observer
+    treeObserver.dispose = treeObserver.disconnect;
+
+    observers.add(atom.workspace.observeTextEditors(function(editor) {
         // mark changes on save
-        editor.buffer.onDidSave(function(file) {
-            atomPerforce.getChanges()
-            .then(function(changes) {
-                atomPerforce.showDiffMarks(file.path, changes);
+        var saveObserver = editor.buffer.onDidSave(function(file) {
+                atomPerforce.getChanges()
+                .then(function(changes) {
+                    atomPerforce.showDiffMarks(file.path, changes);
+                });
             });
+
+        editor.onDidDestroy(function() {
+            saveObserver.dispose();
         });
 
         // mark changes on initial load
@@ -63,27 +84,49 @@ function setupObservers() {
                 atomPerforce.showDiffMarks(editor.getPath(), changes);
             });
         }
+    }));
 
+    observers.add(atom.workspace.observeActivePaneItem(function() {
         atomPerforce.showClientName();
-    });
+    }));
+
+    observers.add(atom.config.onDidChange('atom-perforce.defaultP4Location', setupEnvironment));
 
     atomPerforce.markOpenFiles();
+
+    return observers;
 }
 
 function setupCommands() {
-    ['perforce', 'p4'].forEach(function(prefix) {
-        atom.commands.add('atom-workspace', prefix + ':edit', atomPerforce.edit);
-        atom.commands.add('atom-workspace', prefix + ':add', atomPerforce.add);
-        atom.commands.add('atom-workspace', prefix + ':sync', atomPerforce.sync);
-        atom.commands.add('atom-workspace', prefix + ':revert', atomPerforce.revert);
-        atom.commands.add('atom-workspace', prefix + ':load-opened-files', atomPerforce.loadAllOpenFiles);
-    });
+    if(!commandsSetup) {
+        ['perforce', 'p4'].forEach(function(prefix) {
+            atom.commands.add('atom-workspace', prefix + ':edit', atomPerforce.edit);
+            atom.commands.add('atom-workspace', prefix + ':add', atomPerforce.add);
+            atom.commands.add('atom-workspace', prefix + ':sync', atomPerforce.sync);
+            atom.commands.add('atom-workspace', prefix + ':revert', atomPerforce.revert);
+            atom.commands.add('atom-workspace', prefix + ':load-opened-files', atomPerforce.loadAllOpenFiles);
+        });
+        commandsSetup = true;
+    }
 }
 
 module.exports = {
+    config: {
+        defaultP4Location: {
+            type: 'string',
+            default: getDefaultP4Location()
+        }
+    },
     activate: function activate(/*state*/) {
         setupEnvironment()
-        .then(setupObservers)
-        .then(setupCommands);
+        .then(function() {
+            observers = setupObservers();
+            setupCommands();
+        });
+    },
+    deactivate: function deactivate() {
+        if(observers && observers.dispose) {
+            observers.dispose();
+        }
     }
 };
